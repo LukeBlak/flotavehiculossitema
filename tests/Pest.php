fa9dc103eef1ab871c23ce3fd4d47a77749f87a7
Jorge Echeverria <jfloresjef365@gmail.com>
2026-04-06 09:46:41 -0600

    creacion de test

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,49 +1,34 @@
 <?php
-
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
-/*
-|--------------------------------------------------------------------------
-| Test Case
-|--------------------------------------------------------------------------
-|
-| The closure you provide to your test functions is always bound to a specific PHPUnit test
-| case class. By default, that class is "PHPUnit\Framework\TestCase". Of course, you may
-| need to change it using the "pest()" function to bind a different classes or traits.
-|
-*/
+uses(TestCase::class, RefreshDatabase::class)->in('Feature');
+uses(TestCase::class, RefreshDatabase::class)->in('Unit');
 
-pest()->extend(TestCase::class)
- // ->use(Illuminate\Foundation\Testing\RefreshDatabase::class)
-    ->in('Feature');
-
-/*
-|--------------------------------------------------------------------------
-| Expectations
-|--------------------------------------------------------------------------
-|
-| When you're writing tests, you often need to check that values meet certain conditions. The
-| "expect()" function gives you access to a set of "expectations" methods that you can use
-| to assert different things. Of course, you may extend the Expectation API at any time.
-|
-*/
-
-expect()->extend('toBeOne', function () {
-    return $this->toBe(1);
-});
-
-/*
-|--------------------------------------------------------------------------
-| Functions
-|--------------------------------------------------------------------------
-|
-| While Pest is very powerful out-of-the-box, you may have some testing code specific to your
-| project that you don't want to repeat in every file. Here you can also expose helpers as
-| global functions to help you to reduce the number of lines of code in your test files.
-|
-*/
-
-function something()
-{
-    // ..
+function crearRol(string $nombre): Role {
+    return Role::firstOrCreate(['name' => $nombre, 'guard_name' => 'web']);
 }
+
+function crearPermiso(string $nombre): Permission {
+    return Permission::firstOrCreate(['name' => $nombre, 'guard_name' => 'web']);
+}
+
+function gerente(array $attrs = []): \App\Models\User {
+    return \App\Models\User::factory()->create($attrs)->assignRole(crearRol('gerente'));
+}
+
+function supervisor(array $attrs = []): \App\Models\User {
+    return \App\Models\User::factory()->create($attrs)->assignRole(crearRol('supervisor'));
+}
+
+function motorista(array $attrs = []): \App\Models\User {
+    return \App\Models\User::factory()->create($attrs)->assignRole(crearRol('motorista'));
+}
+
+function vehiculoActivo(): \App\Models\Vehicle {
+    return \App\Models\Vehicle::factory()->create(['status' => 'active']);
+}
+
+beforeEach(function () { app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions(); });
